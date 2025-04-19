091d33dc3046121afef3b7be6f3b3a29498ddeb1
kuntal1984 <74119702+kuntal1984@users.noreply.github.com>
2025-04-19 20:55:07 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cd ~/projects 
-git clone https://github.com/kuntal1984/startup.git
-cp -ar ~/projects/startup/* ~/projects/artifacts/
+proj_path='/home/kuntal_rs/projects'
+datetime=$(date '+%Y-%m-%d%H:%M:%S')
+cd $proj_path
+arti_repos_dir='startup'
+arti_dir='artifacts'
+git_repos_url='https://github.com/kuntal1984'
+
+git clone $git_repos_url/$arti_repos_dir.git
+
+cp -ar $proj_path/$arti_repos_dir/* $proj_path/$arti_dir/
+
+mv  $proj_path/$arti_repos_dir/ $proj_path/$arti_repos_dir_$datetime/
