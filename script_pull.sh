487a8eaaa9840d8bed9f8ffadb134288816770dd
kuntal1984 <74119702+kuntal1984@users.noreply.github.com>
2025-04-21 09:40:28 +0530

    Update script_pull.sh

--- script_pull.sh
+++ script_pull.sh
@@ -6,17 +6,18 @@ script_dir='scripts'
 script_fil='script.sh'
 git_repos_url='https://github.com/kuntal1984'
 datetime=$(date '+%Y-%m-%d%H:%M:%S')
+script_flg = 'script_executed.flg'
 
 
-
-if test -f $proj_path/$script_dir/$script_repos_dir/$script_fil; then
+if test -f $proj_path/$script_dir/$script_flg; then
      echo "File already exist! Do nothing"
-     sh $proj_path/$script_dir/$script_repos_dir/$script_fil
+     
 else
     git clone $git_repos_url/$script_repos_dir.git
         if  test -f $proj_path/$script_dir/$script_repos_dir/$script_fil; then
                 chmod +x $proj_path/$script_dir/$script_repos_dir/$script_fil
                 sh  $proj_path/$script_dir/$script_repos_dir/$script_fil
                 mv  $proj_path/$script_dir/$script_repos_dir/  $proj_path/$script_dir/${script_repos_dir}_${datetime}/
+		touch $proj_path/$script_dir/$script_flg
 	fi
 fi	
